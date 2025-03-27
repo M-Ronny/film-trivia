3dc791e7466fea7d47a290c323c2496c7a715a77
Ronaldo <ronaldomorales.ai@gmail.com>
2025-03-27 16:35:32 -0400

    created index.html, style.css, and main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+fetch("https://opentdb.com/api.php?amount=20&category=11")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
