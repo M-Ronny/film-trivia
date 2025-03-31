d1eba282472e37cb8a94b79721cf1a96eb9853a3
Ronaldo <ronaldomorales.ai@gmail.com>
2025-03-31 16:54:30 -0400

    updated index.html, style.css and add a eventlistener to display the data from api and added a eventlistener to loop though the question

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-fetch("https://opentdb.com/api.php?amount=20&category=11")
+document.querySelector('.startQuiz').addEventListener('click', startQuiz)
+
+function startQuiz() {
+  document.querySelector('.nextQuestion').style.display = 'block'
+  document.querySelector('.startQuiz').style.display = 'none'
+
+  fetch("https://opentdb.com/api.php?amount=20&category=11")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
-      document.querySelector('.question').innerText = data.results[0].question
-      document.querySelector('.a').innerText = `A: ${data.results[0].correct_answer}`
-      document.querySelector('.b').innerText = `B: ${data.results[0].incorrect_answers[0]}`
-      document.querySelector('.c').innerText = `C: ${data.results[0].incorrect_answers[1]}`
-      document.querySelector('.d').innerText = `D: ${data.results[0].incorrect_answers[2]}`
+      document.querySelector('.nextQuestion').addEventListener('click', nextQuestion)
+      let i = 0
+
+      function nextQuestion() {
+        if (i < data.results.length) {
+          document.querySelector('.question').innerText = data.results[i].question
+          document.querySelector('.a').innerText = `A: ${data.results[i].correct_answer}`
+          document.querySelector('.b').innerText = `B: ${data.results[i].incorrect_answers[0]}`
+          document.querySelector('.c').innerText = `C: ${data.results[i].incorrect_answers[1]}`
+          document.querySelector('.d').innerText = `D: ${data.results[i].incorrect_answers[2]}`
+          i++
+        }
+      }
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
+}
